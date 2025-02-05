84c03571da2ab148424aa2cd6c0dc2eaa2283563
Tarachand <tarachand90@gmail.com>
2025-02-05 13:18:00 +0530

    - Added a default message prompting the user to click a button.
    - when the user click the button, it should displayed the correspnding
    tab name instead of default message.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import Header from './components/Header';
 import TabMenu from './components/tabs/TabMenu';
 
 function App() {
+  let tabContent = 'Please click a button';
+
   const onClickHandler = (selectedButton: string) => {
     // Selected button can be Components, JSX, Props and State
+    tabContent = selectedButton;
     console.log(selectedButton);
   };
 
@@ -17,6 +20,7 @@ function App() {
           <CoreConceptList />
           <h2>Examples</h2>
           <TabMenu onTabSelect={onClickHandler} />
+          {tabContent}
         </section>
       </main>
     </div>
